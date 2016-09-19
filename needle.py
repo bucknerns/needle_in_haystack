5a1b563f65ed6db5a8530c13b5e373e4af9e4e62
Nathan Buckner <nathan.buckner@rackspace.com>
2016-09-19 11:59:50 -0500

    Pushed all the things

---
+++ needle.py
@@ -0,0 +1,58 @@
+from collections import Counter
+
+
+def get_min(match, string, last=True):
+    if match is None:
+        return string
+    if len(match) > len(string):
+        return string
+    elif len(match) < len(string):
+        return match
+    elif last:
+        return string
+    else:
+        return match
+
+
+def is_match(n_count, string):
+    return not bool(n_count - Counter(string))
+
+
+class STATES(object):
+    MEND = "MOVE_END"
+    MSTART = "MOVE_START"
+    EXIT = "EXIT"
+
+
+def ninh(needle, haystack):
+    if not needle:
+        return ""
+    if not haystack:
+        return None
+    n_count = Counter(needle)
+    start = 0
+    end = 0
+    match = None
+    state = STATES.MEND
+    while True:
+        if state == STATES.EXIT:
+            return match
+        elif state == STATES.MEND:
+            end += 1
+            if end > len(haystack):
+                state = STATES.EXIT
+                continue
+            if haystack[end - 1] in n_count and is_match(
+                    n_count, haystack[start:end]):
+                match = get_min(match, haystack[start:end])
+                state = STATES.MSTART
+        elif state == STATES.MSTART:
+            start += 1
+            if start == end:
+                state = STATES.MEND
+            if is_match(n_count, haystack[start:end]):
+                match = get_min(match, haystack[start:end])
+            else:
+                start -= 1
+                state = STATES.MEND
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+Created by Nathan Buckner
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="needle-in-haystack",
+    version="0.0.1",
+    description="Needle in haystack interview question",
+    long_description=read("README.md"),
+    author="Nathan Buckner",
+    author_email="dropbox255@gmail.com",
+    url="http://github.com/bucknerns/needle_in_haystack",
+    py_modules=["needle"],
+    include_package_data=True,
+    install_requires=read("pip-requires").split("\n"),
+    license=read("LICENSE"),
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python"))
